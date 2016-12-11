87f51915018b99b6b965b55ca02d3c51d8cc1978
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2016-12-11 20:28:41 +0200

    Implemented new animation for song cartds

--- App/components/SongCard.jsx
+++ App/components/SongCard.jsx
@@ -2,73 +2,51 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '../content/css/song-card.css';
 import SDK from '../soundCloudSDK.jsx';
+import defaultImg from '../content/img/night-default-artwork.png';
 
 class SongCard extends React.Component{
     constructor(props) {
         super(props);
-        this.state = {
-            isActive : false,
-            isLoading: null,
-        }
-    }
-
-    componentWillUpdate(nextProps, nextState){
-        if (this.state.isActive && this.state.isLoading == false && this.props.isCurrent){
-            this.setState({wasPlayed : true});
-        }
     }
 
     componentDidUpdate(prevProps, prevState){
-        if (this.props.isCurrent && !this.state.wasPlayed && !this.state.isActive){
-            this.playLoadAnimation();
+        if (this.props.isCurrent){
+            this.selectSong();
+        } else {
+            this.unSelectSong();
         }
     }
 
-    handleCardClick(event){
+    handleCardClick(){
         this.props.play();
-        this.playLoadAnimation();
+        this.selectSong();
     }
 
-    playLoadAnimation(){
-        this.setState({isActive : true, isLoading:true});
-        setTimeout(() => this.setState({isLoading : false}), 1600);
+    selectSong(){
+        this.card.classList.add('active');
+        this.fab.style.backgroundImage = 'none';
     }
 
-    //get class name associated with appropriate animation
-    getClassName(){
-        const value = "card "; 
-
-        if(this.props.isCurrent){
-            if (this.state.wasPlayed){
-                return value + ' was-played active';
-            } else if (this.state.isActive && this.state.isLoading){
-                return value + ' active';
-            } else if (this.state.isActive && this.state.isLoading == false){
-                return value + ' active now-playing';
-            }
-        } else if (this.state.wasPlayed){
-            return value + ' was-played';
-        }
-
-        return value;
+    unSelectSong(){
+        this.card.classList.remove('active');
+        setTimeout(()=>this.fab.style.backgroundImage = 'url(content/img/card-play.png)', 700)
     }
 
     render() {
-        return <div className={this.getClassName()} 
-                    style={{backgroundImage : "url(" + this.props.artwork + ")"}}>
-                    <h2>{this.props.title}</h2>
+        return <div ref={(ref) => { this.card = ref; }}
+                    className="card">
+                   <div className="main"
+                        style={{backgroundImage : "url(" + this.props.artwork + ")"}}>
+                   </div>
 
-                    <p className="message">Oops, something went wrong :( </p>
-                    <span id="play" className="play" onClick={this.handleCardClick.bind(this)}></span>
-                    <span className="circle-1"></span>
-                    <span className="circle-2"></span>
+                   <div className="fab"
+                        ref = {(ref) => {this.fab = ref}}
+                        onClick={this.handleCardClick.bind(this)}></div>
 
-                    <div className="spinner">
-                        <svg className="load-circle" xmlns="http://www.w3.org/2000/svg" version="1.1" id="Livello_1" x="0px" y="0px" width="39.053px" height="36.653px" viewBox="0 0 39.053 36.653" enableBackground="new 0 0 39.053 36.653">
-                            <path fill="none" stroke="#fff" strokeWidth="3" strokeLinecap="round" strokeMiterlimit="10" d="M11.989,32.312  c-4.463-2.599-7.463-7.435-7.463-12.972c0-8.283,6.716-15,15-15s15,6.717,15,15"></path>
-                        </svg>
-                    </div>
-                </div>
+                   <div className="fab-sha"></div>
+                   <div className="close"
+                        onClick={this.unSelectSong.bind(this)}></div>
+               </div>
     }
 }
  
