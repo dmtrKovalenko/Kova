a789514eb22172734cbee678c7be9447418c48d2
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2017-05-28 00:25:53 +0300

    deploy

--- bin/server.js
+++ bin/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const project = require('../config/project.config')
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.static(project.dir_dist));
 
