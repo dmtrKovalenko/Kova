857c4ddbb79ae52ce144cae43ec562ae15d3e003
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2017-05-28 14:11:48 +0300

    d

--- bin/server.js
+++ bin/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 8080;
 app.use(express.static(project.dir_dist));
 
 app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/../dist/index.html');
+  response.sendFile(path.resolve(__dirname + '/../dist/index.html'));
 });
 
 app.listen(PORT, error => {
